d95dff6fb52520c0a1c81561d9af03e996efeaaf
Priyanshu Shubham <priyanshushubham27@gmail.com>
2020-05-27 04:43:31 +0530

    added foundation of encrypt method and added methods fromString and str ❤

--- password.py
+++ password.py
@@ -1,25 +1,42 @@
 class Password:
     normalChars = list(
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#_")
-    specialChars = list("~`! @#$%^&*()_-+={[}]|\:;\"'<,>.?/")
+    specialChars = list("""~`!@#$%^&*()_-+={[}]|\:;"'<,>.?/""")
 
-    def __init__(self, password=""):
-        self.password = password
+    def __init__(self, symbolSequence, length):
+        self.password = self.getPassword(symbolSequence, length)
 
     def getPassword(self, symbolSequence, length):
         """Returns a new password of length=length with characters from symbolSequence"""
         symbolArray = list(symbolSequence)
         import random
-        password = ""
+        self.password = ""
         # initializing with a new password
         intpos = [i for i in range(length)]
         random.shuffle(intpos)
         n = random.randint(1, length//4)
         # atleast n places where integer will be put depending on the first n values in intpos.
         for i in range(length):
-            password += symbolArray[random.randint(0, len(symbolArray)-1)]
+            self.password += symbolArray[random.randint(0, len(symbolArray)-1)]
         # putting in n integers
         for i in range(n):
-            password = password[:intpos[i]] + \
-                str(random.randint(0, 9))+password[intpos[i]+1:]
-        return password
+            self.password = self.password[:intpos[i]] + \
+                str(random.randint(0, 9))+self.password[intpos[i]+1:]
+        return self.password
+
+    def encrypt(self, key):
+        from cryptography.fernet import Fernet
+        pass
+
+    @classmethod
+    def fromString(cls, password):
+        instance = cls(cls.normalChars, 5)
+        instance.password = password
+        return instance
+
+    def __str__(self):
+        return self.password
+
+
+p1 = Password.fromString("HeyBro")
+print(p1)
