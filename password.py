7b8fb46da17a4ad298c6775fca009386bd8e7a9e
Priyanshu Shubham <priyanshushubham27@gmail.com>
2020-05-27 12:24:30 +0530

    added validate,getAuthoriser and getKey method

--- password.py
+++ password.py
@@ -5,6 +5,8 @@ class Password:
 
     specialChars = list("""~`!@#$%^&*()_-+={[}]|\:;"'<,>.?/""")
 
+    __isValidated = 0
+
     def __init__(self, symbolSequence, length):
         self.password = self.getPassword(symbolSequence, length)
 
@@ -27,22 +29,84 @@ class Password:
         return self.password
 
     def encrypt(self, key):
+        """Encrypt the self.password"""
         from cryptography.fernet import Fernet
-        pass
+        f = Fernet(key)
+        token = f.encrypt(self.password.encode()).decode()
+        self.token = token
+        return token
 
     @classmethod
     def fromString(cls, password):
+        """Initialize the Password class from string."""
         instance = cls(cls.normalChars, 5)
         instance.password = password
         return instance
 
     @classmethod
     def fromToken(cls, token):
+        """Initialize the Password Class from Token."""
         pass
 
     def __str__(self):
         return self.password
 
+    @staticmethod
+    def getKey(masterPassword):
+        """Returns Fermet.key from the master Password."""
+        import base64
+        from cryptography.fernet import Fernet
+        from cryptography.hazmat.backends import default_backend
+        from cryptography.hazmat.primitives import hashes
+        from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+        password = masterPassword.encode()
+        import secret
+        salt = secret.salt
+        # binary sat using os.urandom(16)
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+            backend=default_backend()
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(password))
+        return key
+
+    @classmethod
+    def validate(cls):
+        import getpass
+        from cryptography.fernet import Fernet
+        import secret
+        # Flag:There can be problems here since maybe the fetchone method in getAuthoriser returns a tuple in place of a string.
+        authoriser = Password.getAuthoriser()
+        for i in range(3):
+            master = getpass.getpass(prompt="Enter Master Password:")
+            key = Password.getKey(master)
+            f = Fernet(key)
+            _auth = f.decrypt(authoriser.encode())
+            if _auth == secret.authoriser:
+                Password.__isValidated = 1
+                return key
+            else:
+                print("Wrong Password")
+                continue
+        import sys
+        sys.exit(0)
+
+    @staticmethod
+    def getAuthoriser():
+        """Gets the authoriser text stored in database "passwords" which is encrypted"""
+        import sqlite3
+        conn = sqlite3.connect("passwords")
+        cur = conn.cursor()
+        result = cur.execute("SELECT * FROM authoriser")
+        authoriser = cur.fetchone()[0]
+        # Flag : What cursor.fetchone returns.
+        print(authoriser)
+        conn.close()
+        return authoriser
+
 
 p1 = Password.fromString("HeyBro")
-print(p1)
+print(p1.getKey("HElooThere"))
